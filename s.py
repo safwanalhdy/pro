578650f8b1fd26e3385d0bad5a3ea161be0ca3a4
safwanalhdy <sfwanalhdy1@gmail.com>
2024-10-03 20:14:10 +0300

    Add files via upload

---
+++ s.py
@@ -0,0 +1,11 @@
+def check_number(number):
+    if number % 2==0:
+        pring(f"{number}هو عدد زوجي")
+    else:
+        print(f"{number}هو عدد فردي")
+
+def check_number(number):
+    if number % 2==0:
+        pring(f"{number}هو عدد زوجي")
+    else:
+        print("yes")
